0576f23c31bced88ad5d4facbd9b8e01ee5198aa
dfdfdfdf129 <iordache.tudor129@gmail.com>
2025-09-04 10:35:25 +0300

    constants thing

---
+++ System Design y2 python group constants,reqs and assumptions.py
@@ -0,0 +1,70 @@
+#Import
+import math
+import numpy as np
+
+#assumptions
+massratioL = 0.925
+
+
+
+#Constants
+g = 9.81
+
+#Requirements
+PLreq = 9302 #Payload (kg)
+TOreq = 1296 #Takeoff (m)
+LDreq = 1210 #Landing (m)
+CRreq = 0.77 #Cruise (Mach)
+Vcr_TAS = 228.332
+Vcr_EAS = 127.104
+R_des = 2019
+#Class I weight estimation
+MTOM = 38939.25
+OEM = 22694.2
+Mp = 9302
+MF = MTOM - OEM - Mp 
+ef = 44000000 
+R_div = 250  #?
+#wing
+AR = 9
+L = g
+cf = 0.0027
+SwetpS = 6
+CLmax_cruise = 1.5
+CLmax_Takeoff = 1.9
+CLmax_Landing = 2.3
+V_stall_requirement = 1
+V_appro = 1.23 * V_stall_requirement
+Cd0 = cf*SwetpS
+Cd = 2*Cd0
+nj = (Vcr_TAS/(TSFC/1000000))/ef
+
+#PW1519G
+ThrustPerEngine = 88 #kN
+TSFC = 11.3 #g/(kNs)
+njf = 0.46
+BPR = 12
+
+#Stats
+
+
+
+#Code
+
+
+#Minimum speed formula
+
+
+#Landing field Length formula
+
+
+#Cruise speed formula
+
+
+#Climb rate formula 
+
+
+#Climb gradient requirements formulas
+
+
+#Take-off field length formula
