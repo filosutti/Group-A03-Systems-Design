6cd096f8265016e87044261193e4e7e9f33f9f54
albanrutz <albanrutz@proton.me>
2025-09-08 15:53:52 +0200

    Update minspeed.py

--- minspeed.py
+++ minspeed.py
@@ -5,4 +5,4 @@ def minSpeed(CLmax_Takeoff, massfraction, V_appro):
     density = 1.225 #kg/m^3 at sea level
     min = (1/massfraction)*(density/2)*((V_appro/1.23)**2)*(CLmax_Takeoff)
 
-    return min 
+    return min
