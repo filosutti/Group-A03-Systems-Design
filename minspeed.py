04ac48ab6056bcd38962f09e49e2aa8b07e3c4fb
albanrutz <albanrutz@proton.me>
2025-09-08 15:52:15 +0200

    Update minspeed.py

--- minspeed.py
+++ minspeed.py
@@ -1,9 +1,8 @@
 #MINIMUM SPEED
 #I think it's CLmax_Takeoff but might be CLmax_Landing
 
-def minSpeed(CLmax_Takeoff, massfraction, V_appro): 
-    density = 1.225
-
+def minSpeed(CLmax_Takeoff, massfraction, V_appro):
+    density = 1.225 #kg/m^3 at sea level
     min = (1/massfraction)*(density/2)*((V_appro/1.23)**2)*(CLmax_Takeoff)
 
-    return min
+    return min 
