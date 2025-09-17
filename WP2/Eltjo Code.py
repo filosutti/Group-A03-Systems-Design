a71f5f26c09ff3fc70b9597d1f3778c7bfb7e82e
erietman46 <e.rietman@student.tudelft.nl>
2025-09-17 14:54:10 +0200

    c(y)

--- WP2/Eltjo Code.py
+++ WP2/Eltjo Code.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-M_cruise = 0.77
 
-sweep_angle_quarter_chord = np.arccos(1.16 / (M_cruise + 0.5))
-sweep_angle_quarter_chord_deg = np.degrees(sweep_angle_quarter_chord)
-taper_ratio = 0.2*(2-sweep_angle_quarter_chord_deg*(np.pi/180))
+def c(y)
+    c = 4.27-0.230921312*y
+    return(c)
+
 
 
 
