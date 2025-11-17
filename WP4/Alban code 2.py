989219b690680da9a678df4b17e9fdecdfc50d86
albanrutz <albanrutz@proton.me>
2025-11-17 15:00:46 +0100

    Update Alban code 2.py

--- WP4/Alban code 2.py
+++ WP4/Alban code 2.py
@@ -7,11 +7,18 @@ import matplotlib.pyplot as plt
 
 print("i love kyrgyzstan")
 
-corner = [(0,0), (4,0), (3,2), (1,2)]
+# corner inputs (temporary)
+corner = []
+corners = 4
+for i in range(corners):
+    print(f"Enter coordinates for corner {i+1}:")
+    x = float(input("  x: "))
+    y = float(input("  y: "))
+    corner.append((x, y))
 
 def WingBoxCentroid(corner):
-    cx = (corner[0][0] + corner[1][0] + corner[2][0] + corner[3][0]) / 4
-    cy = (corner[0][1] + corner[1][1] + corner[2][1] + corner[3][1]) / 4
+    cx = (corner[0][0] + corner[1][0] + corner[2][0] + corner[3][0]) / corners
+    cy = (corner[0][1] + corner[1][1] + corner[2][1] + corner[3][1]) / corners
     print("Coordinates (x, y) of wing box centroid: (", cx, ",", cy, ")")
     return cx, cy
 
