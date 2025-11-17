7109506871168ead5a2287ec0a0ddbcb89826237
lguisset <l.c.j.guisset@student.tudelft.nl>
2025-11-17 16:57:11 +0100

    new

--- WP4/TL.py
+++ WP4/TL.py
@@ -6,10 +6,9 @@ cm_list = []
 import os
 import sys
 
-sys.path.append('..')
 
 
-with open ('C:/Users/iorda/OneDrive/Desktop/GitHub/Group-A03-System-Design/WP4/XFLR_a_0.txt', 'r') as f:
+with open ('WP4/XFLR0.txt','r') as f:
     for line in f:
         if line.strip() == line.startswith("y-span") or len(line.split()) < 10 or float(line.split()[0]) < 0:
             continue
@@ -18,8 +17,8 @@ with open ('C:/Users/iorda/OneDrive/Desktop/GitHub/Group-A03-System-Design/WP4/X
 
         y = float(parts[0])
         cl = float(parts[3])
-        cd = float(parts[5])   # total Cd? might want PCd+ICd depending on your use
-        cm = float(parts[7])   # airfoil pitching moment at c/4
+        cd = float(parts[5])   
+        cm = float(parts[7])   
 
         y_list.append(y)
         cl_list.append(cl)
