bada57e777b92f00b5e2f08d1c0a4947035ecc2b
filosutti <filippo.sutti@icloud.com>
2025-11-18 14:03:17 +0100

    update shear diagram

--- WP4/loading diagrams/sheardiagram.py
+++ WP4/loading diagrams/sheardiagram.py
@@ -17,25 +17,29 @@ n_choose = n_pos
 #-------------------------------------------------------------------------------------
 
 
+#Define Point Loads
+#-------------------------------------------------------------------------------------
+W_engine_NOLOAD = 2177.243*g
+
+
 #Define Distributed Loads
 #-------------------------------------------------------------------------------------
 def LiftDistribution(x, n):
     return (x)*n
 
 def WeightDistribution(x,n):
-    return ((7*x)-821.693393608074)*n
+    return (7*x-821.693393608074)*n
 
 def FuelDistribution(x):
-    return x
+    return (10*x-9148.6175)
 
-def w(x):
-    return LiftDistribution(x,n_choose) - WeightDistribution(x,n_choose) - FuelDistribution(x)
-
-
-#Define Point Loads
+#Distributed Loading in one function
 #-------------------------------------------------------------------------------------
-W_engine_NOLOAD = 2177.243*g
+def w(x):
+    return LiftDistribution(x,n_choose) - WeightDistribution(x,n_choose) - FuelDistribution(x) - W_engine_NOLOAD
 
 
 
 ShearEstimate, error1 = scipy.integrate.quad(w, 0, winghalfspan)
+print(ShearEstimate)
+
