a7a368eb37efc5f15ed90be9bc18981162f6bae6
albanrutz <albanrutz@proton.me>
2025-12-01 14:07:09 +0100

    Update finaldeflection.py

--- WP4/finaldeflection.py
+++ WP4/finaldeflection.py
@@ -4,4 +4,9 @@ import matplotlib.pyplot as plt
 import alban_code_4 as A
 import bendingdiagramnegativeload as Bneg
 import bendingdiagrampositiveload as Bpos
+import sheardiagramNEGATIVEloadfactor as Sneg
+import sheardiagramPOSITIVEloadfactor as Spos
+import Ultimate_Julian_code_3 as J
+
+
 
