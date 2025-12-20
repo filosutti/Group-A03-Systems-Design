ab2751ff2dd32fafa2790e7678bf44d648b713b8
dfdfdfdf129 <iordache.tudor129@gmail.com>
2025-12-20 13:25:00 +0100

    Update Skin_buckling.py

--- WP4/Skin_buckling.py
+++ WP4/Skin_buckling.py
@@ -93,7 +93,7 @@ def crit_buckling_stress1(nr_ribs, case):
 #function for linear spacing diff below:
 
 def crit_buckling_stress2(nr_ribs, case, initial_spacing):
-    ylst2, spacings = rib_places(initial_spacing, 11.89, nr_ribs, 0)
+    ylst2, spacings = rib_places(initial_spacing, 11.89, nr_ribs, 1.5)
     print(ylst2)
     margin_of_safety2 = []
     if(case == 1):
@@ -130,7 +130,7 @@ a_eq = wingspan / (2 * (nr_ribs - 1))
 ylst1 = np.array([a_eq * i for i in range(nr_ribs - 1)])
 mos1 = crit_buckling_stress1(nr_ribs, case)
 
-ylst2_ribs, _ = rib_places(initial_spacing, wingspan/2, nr_ribs, 0)
+ylst2_ribs, _ = rib_places(initial_spacing, wingspan/2, nr_ribs, 1.5)
 ylst2_bays = ylst2_ribs[:-2]   # one per bay
 mos2 = crit_buckling_stress2(nr_ribs, case, initial_spacing)
 
